edd759a6fa4f552c24dd584f6f9c6dab48dc9684
Oded <oded@fito.ai>
2025-10-27 17:22:27 +0200

    Update Gradle, Kotlin, and add namespace for AGP 8+ compatibility

--- android/src/main/kotlin/it/aesys/flutter_cast_video/FlutterVideoCastPlugin.kt
+++ android/src/main/kotlin/it/aesys/flutter_cast_video/FlutterVideoCastPlugin.kt
@@ -1,63 +1,44 @@
 package it.aesys.flutter_cast_video
 
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** FlutterVideoCastPlugin */
 public class FlutterVideoCastPlugin: FlutterPlugin, ActivityAware {
-  private lateinit var chromeCastFactory: ChromeCastFactory
+    private lateinit var chromeCastFactory: ChromeCastFactory
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    chromeCastFactory = ChromeCastFactory(flutterPluginBinding.binaryMessenger)
-    flutterPluginBinding
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        chromeCastFactory = ChromeCastFactory(flutterPluginBinding.binaryMessenger)
+        flutterPluginBinding
             .platformViewRegistry
             .registerViewFactory(
-                    "ChromeCastButton",
-                    chromeCastFactory
+                "ChromeCastButton",
+                chromeCastFactory
             )
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      registrar
-              .platformViewRegistry()
-              .registerViewFactory(
-                      "ChromeCastButton",
-                      ChromeCastFactory(registrar.messenger())
-              )
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    
-  }
+    // Remove the companion object entirely - it's deprecated and not needed anymore
+    // The new embedding (onAttachedToEngine) is sufficient
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    chromeCastFactory.activity = binding.activity
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
 
-  override fun onDetachedFromActivityForConfigChanges() {
+    }
 
-  }
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        chromeCastFactory.activity = binding.activity
+    }
 
-  override fun onDetachedFromActivity() {
+    override fun onDetachedFromActivityForConfigChanges() {
 
-  }
+    }
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    override fun onDetachedFromActivity() {
 
-  }
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+
+    }
 }
